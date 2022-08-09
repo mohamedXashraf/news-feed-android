be70b4dcb2b905472c0df730a864e409b6a1136a
Mohamed Ashraf <mohamed.ashraf.10107@gmail.com>
2022-08-09 16:14:22 +0200

    Commit #3: model & news API.

--- data/src/main/java/com/linkdevelopment/data/api/NewsAPIs.kt
+++ data/src/main/java/com/linkdevelopment/data/api/NewsAPIs.kt
@@ -1,4 +1,18 @@
 package com.linkdevelopment.data.api
 
+import com.linkdevelopment.data.base.Response
+import com.linkdevelopment.model.News
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsAPIs {
+
+    private val key: String
+        get() = "533af958594143758318137469b41ba9"
+
+    @GET("articles")
+    suspend fun getArticles(
+        @Query("source") source: String,
+        @Query("apiKey") apiKey: String = key
+    ): Response<News>
 }
--- model/src/main/java/com/linkdevelopment/model/News.kt
+++ model/src/main/java/com/linkdevelopment/model/News.kt
@@ -1,4 +1,12 @@
 package com.linkdevelopment.model
 
-class News {
-}
+import com.google.gson.annotations.SerializedName
+
+data class News(
+    @SerializedName("author") var author: String? = "",
+    @SerializedName("title") var title: String? = "",
+    @SerializedName("description") var description: String? = "",
+    @SerializedName("url") var url: String? = "",
+    @SerializedName("urlToImage") var urlToImage: String? = "",
+    @SerializedName("publishedAt") var publishedAt: String? = ""
+)
